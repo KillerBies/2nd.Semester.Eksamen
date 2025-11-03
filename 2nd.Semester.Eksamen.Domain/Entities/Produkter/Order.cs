09938a323d3b329fa5b968a81726f08708099541
Kodeism <emilgeertsenucl@gmail.com>
2025-11-03 17:44:53 +0100

    Tried Sketching some domain classes

---
+++ 2nd.Semester.Eksamen.Domain/Entities/Produkter/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd.Semester.Eksamen.Domain.Entities.Produkter
+{
+    public class Order : Base_Entity
+    {
+        public Booking Booking { get; set; }
+        public List<OrderProdukt> Produkter { get; set; }
+    }
+}
