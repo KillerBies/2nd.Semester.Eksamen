828195f58ee2ef7746a65ee212c6b8d7891574e0
Johannes Caesar Melton <johannescj@gmail.com>
2025-11-17 09:44:37 +0100

    Comment

--- 2nd.Semester.Eksamen.Domain/Entities/BaseEntity.cs
+++ 2nd.Semester.Eksamen.Domain/Entities/BaseEntity.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _2nd.Semester.Eksamen.Domain.Entities
+﻿namespace _2nd.Semester.Eksamen.Domain.Entities
 {
     public abstract class BaseEntity
     {
+        //Base entity
         public int Id { get; private set; }
     }
 }
