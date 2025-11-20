fe66f41e3ad5554b605f7856955296b2ad5b35df
Johannes Caesar Melton <johannescj@gmail.com>
2025-11-20 09:33:09 +0100

    Yes

--- 2nd.Semester.Eksamen.Domain/Entities/Persons/Customer.cs
+++ 2nd.Semester.Eksamen.Domain/Entities/Persons/Customer.cs
@@ -1,11 +1,6 @@
 ﻿using _2nd.Semester.Eksamen.Domain.Entities.Products;
 using _2nd.Semester.Eksamen.Domain.Entities.Tilbud;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _2nd.Semester.Eksamen.Domain.Entities.Persons
 {
@@ -14,13 +9,13 @@ namespace _2nd.Semester.Eksamen.Domain.Entities.Persons
     {
         //Base class for customers
         public List<Booking> BookingHistory { get; private set; } = new List<Booking>();
-        public int NumberOfVisists { get; private set; } 
-        public decimal PointBalance { get; private set; } 
+        public int NumberOfVisists { get; private set; }
+        public decimal PointBalance { get; private set; }
         public List<PunchCard> PunchCards { get; private set; } = new List<PunchCard>();
         public string Notes { get; set; } = string.Empty;
 
         public Customer() { }
-        public Customer(string name, Address address, string phoneNumber, string email) : base(name, address, phoneNumber, email) 
+        public Customer(string name, Address address, string phoneNumber, string email) : base(name, address, phoneNumber, email)
         {
             BookingHistory = new List<Booking>();
             PointBalance = 0;
@@ -35,7 +30,7 @@ namespace _2nd.Semester.Eksamen.Domain.Entities.Persons
         //method to change point balance
         public bool TryAddToPointBalance(decimal points)
         {
-            if(points < 0) return false;
+            if (points < 0) return false;
             PointBalance += points;
             return true;
         }
@@ -45,7 +40,7 @@ namespace _2nd.Semester.Eksamen.Domain.Entities.Persons
         //method to redeem points from point balance
         public bool TryRedeemPoints(decimal points)
         {
-            if(points <= PointBalance)
+            if (points <= PointBalance)
             {
                 PointBalance -= points;
                 return true;
@@ -55,10 +50,10 @@ namespace _2nd.Semester.Eksamen.Domain.Entities.Persons
 
 
 
-        //method to add to booking history
+        //method to add to booking history.
         public bool TryAddToBookingHistory(Booking booking)
         {
-            if(booking.Status == BookingStatus.Completed)
+            if (booking.Status == BookingStatus.Completed)
             {
                 BookingHistory.Add(booking);
                 return true;
@@ -71,7 +66,7 @@ namespace _2nd.Semester.Eksamen.Domain.Entities.Persons
         //method to add a punchcard
         public bool TryAddPunchCard(Treatment treatment)
         {
-            if(!PunchCards.Any(Card=>Card.Treatment==treatment))
+            if (!PunchCards.Any(Card => Card.Treatment == treatment))
             {
                 PunchCards.Add(new PunchCard(this, 1, treatment));
                 return true;
