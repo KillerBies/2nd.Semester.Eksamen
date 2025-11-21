32a84a55b7a05afdd234f09838cee0eb4ce178e0
Johannes Caesar Melton <johannescj@gmail.com>
2025-11-21 13:37:20 +0100

    .

--- 2nd.Semester.Eksamen.Application/Services/PrivateCustomerService.cs
+++ 2nd.Semester.Eksamen.Application/Services/PrivateCustomerService.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using _2nd.Semester.Eksamen.Application.ApplicationInterfaces;
 using _2nd.Semester.Eksamen.Application.DTO;
-using _2nd.Semester.Eksamen.Application.ApplicationInterfaces;
-using _2nd.Semester.Eksamen.Domain.Entities.Persons;
 using _2nd.Semester.Eksamen.Application.RepositoryInterfaces;
+using _2nd.Semester.Eksamen.Domain.Entities.Persons;
 namespace _2nd.Semester.Eksamen.Application.Services
 {
     public class PrivateCustomerService : IPrivateCustomerService
-    { 
-      
-       private readonly IPrivateCustomerRepository _customerRepository;
-       public PrivateCustomerService(IPrivateCustomerRepository customerRepository)
+    {
+        private readonly IPrivateCustomerRepository _customerRepository;
+        public PrivateCustomerService(IPrivateCustomerRepository customerRepository)
         {
             _customerRepository = customerRepository;
         }
@@ -29,12 +23,12 @@ namespace _2nd.Semester.Eksamen.Application.Services
             //Creates Address for private customer
             Address address = new Address(DTO.City, DTO.PostalCode, DTO.StreetName, DTO.HouseNumber);
             //Creates Private Customer
-             PrivateCustomer privateCustomer = new PrivateCustomer(DTO.LastName, DTO.Gender, DTO.Birthday, DTO.FirstName, address, DTO.PhoneNumber, DTO.Email);
-             _customerRepository.CreateNewAsync(privateCustomer); 
+            PrivateCustomer privateCustomer = new PrivateCustomer(DTO.LastName, DTO.Gender, DTO.Birthday, DTO.FirstName, address, DTO.PhoneNumber, DTO.Email);
+            _customerRepository.CreateNewAsync(privateCustomer);
         }
 
-        
-        }
+
+    }
 
 
 }
